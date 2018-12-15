518798efb221498945a3f9b031dbe5a791c8d596
Jack Bransfield <bransfieldjack@gmail.com>
2018-12-15 13:34:18 +0000

    Added index.html route to react index page in django urls.py.

---
+++ infoexchange_react/build/precache-manifest.ee4ae720323c18a1e64fa0322b2e3ba7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d860e6705485740f429c",
+    "url": "/static/js/main.d860e670.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "d860e6705485740f429c",
+    "url": "/static/css/main.aee2245a.chunk.css"
+  },
+  {
+    "revision": "f9491c98b3e702f4895364f00a229493",
+    "url": "/index.html"
+  }
+];
