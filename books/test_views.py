7ab856ac7069f579bcaef36e919446c99d39f488
Jack Bransfield <bransfieldjack@gmail.com>
2018-12-16 08:38:06 +0000

    Updated requirements.txt file and removed unused test files.

--- books/test_views.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-
-class TestViews(TestCase):
-    
-    
-    def test_get_home_page(self):
-        page = self.client.get("/")
-        self.assertEqual(page.status_code, 200)
