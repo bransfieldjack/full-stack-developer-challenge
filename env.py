d117044a15089ce52f598546efa35afe8d0b28fc
Jack Bransfield <bransfieldjack@gmail.com>
2018-12-17 10:26:31 +0000

    env file was blank for some reason, adding the variables back in.

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault('STRIPE_PUBLISHABLE', 'pk_test_IPY2fGRoZkPTnPvrX1YC8WDH')
+os.environ.setdefault('DATABASE_URL', 'postgres://nbtdlnvbeoqepr:3ec516b802db5c30d70071f748ac9fab30b2f0324337b2f090f1dba22f32efa1@ec2-23-21-65-173.compute-1.amazonaws.com:5432/d2kgg9ki91vlcf')
+os.environ.setdefault('SECRET_KEY', '^9%j^kybjolf@xo!!_$b-4qvuh-2bezpntz^5*vq4tkpzujp65')
