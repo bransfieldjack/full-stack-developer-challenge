4a14ae6534148064e6718811be6c7aa49a0e6cbb
Jack Bransfield <bransfieldjack@gmail.com>
2018-12-19 03:21:32 +0000

    Updated database url in settings.py.

--- infoexchange/settings.py
+++ infoexchange/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['info-exchange-app.herokuapp.com', 
-                 'info-exchange-app-jackalack117.c9users.io']
+ALLOWED_HOSTS = ['info-exchange-app.herokuapp.com']
 
 
 # Application definition
@@ -81,14 +80,14 @@ WSGI_APPLICATION = 'infoexchange.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
-#DATABASES = {'default': dj_database_url.parse('postgres://nbtdlnvbeoqepr:3ec516b802db5c30d70071f748ac9fab30b2f0324337b2f090f1dba22f32efa1@ec2-23-21-65-173.compute-1.amazonaws.com:5432/d2kgg9ki91vlcf')}
+DATABASES = {'default': dj_database_url.parse('postgres://nbtdlnvbeoqepr:3ec516b802db5c30d70071f748ac9fab30b2f0324337b2f090f1dba22f32efa1@ec2-23-21-65-173.compute-1.amazonaws.com:5432/d2kgg9ki91vlcf')}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
